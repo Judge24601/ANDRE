788e9d259f9af7726a78cb2c1d0180f893f86c20
Alex Ezzat <alexezzat@dhcp-128-189-244-238.ubcsecure.wireless.ubc.ca>
2018-02-12 17:13:06 -0800

    C++ code project

---
+++ ArduinoCode/RCCode.cpp
@@ -0,0 +1,76 @@
+""" RC car code"""
+
+objectAvoidanceDistance = 3;
+
+rightV = 0;
+leftV = 255;
+straightV = (leftV + rightV)/2;
+
+#Front IR Sensor
+int frontSensorPin = 0;
+
+#Side IR Sensor
+int leftSensorPin = 1;
+int rightSensorPin = 2;
+
+#Set main motor to high
+int driveTrainPin = 0;
+pinMode(driveTrainPin, OUTPUT);
+
+
+#start running superloop
+while(1) {
+
+    #Read front IR sensor
+    dist = analogRead(frontSensorPin);
+
+    if(dist > objectAvoidanceDistance) {
+        avoid();
+    }
+}
+
+void avoid() {
+    #Turn front wheels right
+    GPIO.output(0, rightV)
+    
+    """Perform 90deg turn"""
+    
+    #Turn front wheels to straight
+    GPIO.output(0, straightV)
+    
+    #Keep straight while side sensor reads high voltage
+    while(GPIO.input(2) > clearDistance):
+    #Keep going forwards
+        
+        """After side is clear"""
+        
+    #Turn front wheels left
+        GPIO.output(0, leftV)
+        
+        """Perform 90deg turn"""
+        
+    #Turn front wheels straight
+        GPIO.output(0, straightV)
+        
+        """Car now parallel to object, headed in correct direction"""
+        
+    #Keep reading same side sensor
+        while(GPIO.input(2) > clearDistance):
+            
+            """After object has been passed, turn back on to correct path"""
+            
+    #Turn front wheels left
+            GPIO.output(0, leftV)
+            
+    #Go some correction distance
+            
+    #Turn front wheels right, back on to correct path
+            GPIO.output(0, rightV)
+            
+            """Perform 90deg turn"""
+            
+    #Set front wheels straight
+            GPIO.output(0, straightV)
+            
+    #end avoid() method
+            }
---
+++ ArduinoCode/RCCode.hpp
@@ -0,0 +1,13 @@
+//
+//  RCCode.hpp
+//  ArduinoCode
+//
+//  Created by Alex Ezzat on 2018-02-12.
+//
+
+#ifndef RCCode_hpp
+#define RCCode_hpp
+
+#include <stdio.h>
+
+#endif /* RCCode_hpp */
