3cdd222c6b9af5f745b1f6b605b4735cbbbfe842
Alex Ezzat <alexezzat@dhcp-128-189-244-238.ubcsecure.wireless.ubc.ca>
2018-02-12 17:41:30 -0800

    Adaption to C++ done

--- ArduinoCode/RCCode.cpp
+++ ArduinoCode/RCCode.cpp
@@ -33,47 +33,49 @@ while(1) {
 }
 
 void avoid() {
-    #Turn front wheels right
+#Turn front wheels right
     analogWrite(servoPin, rightV);
     
     """Perform 90deg turn"""
     
-    #Turn front wheels to straight
+#Turn front wheels to straight
     analogWrite(servoPin, straightV);
     
-    #Keep straight while side sensor reads high voltage
-    while(analogRead(leftSensorPin) > clearDistance):
-    #Keep going forwards
+#Keep straight while side sensor reads high voltage
+    while(analogRead(leftSensorPin) > clearDistance) {
+#Keep going forwards
         
         """After side is clear"""
+    }
+    
+#Turn front wheels left
+    analogWrite(servoPin, leftV);
+    
+    """Perform 90deg turn"""
+    
+#Turn front wheels straight
+    analogWrite(servoPin, straightV);
+    
+    """Car now parallel to object, headed in correct direction"""
+    
+#Keep reading same side sensor
+    while(GPIO.input(2) > clearDistance){
         
-    #Turn front wheels left
-        GPIO.output(0, leftV)
-        
-        """Perform 90deg turn"""
-        
-    #Turn front wheels straight
-        GPIO.output(0, straightV)
-        
-        """Car now parallel to object, headed in correct direction"""
-        
-    #Keep reading same side sensor
-        while(GPIO.input(2) > clearDistance):
-            
-            """After object has been passed, turn back on to correct path"""
-            
-    #Turn front wheels left
-            GPIO.output(0, leftV)
-            
-    #Go some correction distance
-            
-    #Turn front wheels right, back on to correct path
-            GPIO.output(0, rightV)
-            
-            """Perform 90deg turn"""
-            
-    #Set front wheels straight
-            GPIO.output(0, straightV)
-            
-    #end avoid() method
-            }
+    }
+    """After object has been passed, turn back on to correct path"""
+    
+#Turn front wheels left
+    analogWrite(servoPin, leftV);
+    
+#Go some correction distance
+    
+#Turn front wheels right, back on to correct path
+    analogWrite(servoPin, rightV);
+    
+    """Perform 90deg turn"""
+    
+#Set front wheels straight
+    analogWrite(servoPin, straightV);
+    
+#end avoid() method
+}
