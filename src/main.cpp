dae877ce325f9be0644889008e57780f9511ed02
Donney Fan <donney.fan@hotmail.com>
2018-03-20 13:13:16 -0700

    Updated main logic

--- src/main.cpp
+++ src/main.cpp
@@ -1,116 +1,144 @@
 /*
-Into Robotics
-*/
- 
-#include <servo.h>  //add '<' and '>' before and after servo.h
+ * ANDRE Main code logic.
+ */
+
+#include <servo.h> 
 #include <Arduino.h>
 // #include <PulseInZero.h>  // try Github repo to pulsein without delay
 
-#define LEDPin 13 // Onboard LED
-int receiver_pin = 8;
-int servoPin = 2;
-
-bool range = false;
 bool turned = false;
 
-// defines pins numbers
+// Define pins numbers
 const int motorPin = 7;
 const int trigPin = 9;
 const int echoPin = 10;
-// defines variables
-int maximumRange = 30; // Maximum range needed
-int minimumRange = 0; // Minimum range needed
+const int servoPin = 2;
+const int IRPinLeft = 5;
+const int IRPinRight = 6;
+
+// Other constants
+const int defaultServoAngle = 65; // Reference angle in which the wheels are aligned straight
+const int bitToMotorRPM = 0; // Conversion factor from an 8-bit number (0 to 255) to some speed of the motor
+const int maxServoAngle = 0; // Maximum angle from straight in which the servo can comfortably rotate.
+
+// Global variables to store data
+int maximumRange = 30; // Maximum range threshold for sonar
+int minimumRange = 0; // Minimum range threshold for sonar
 long duration, distance; // Duration used to calculate distance
-bool motorOn = false; 
-Servo servo;  
+Servo servo;
+bool motorOn = false;
 int motorCounter = 0;
-int servoAngle = 0;   // servo position in degrees
- 
-void setup()
-{
-  Serial.begin(9600);  
+int servoAngle = 0; // servo position in degrees
+
+// Function prototypes
+void turnAbs(int angle);
+void turnRef(int angle);
+void turnStraignt();
+void getSonarData();
+void changeMotorSpeed(int RPM);
+
+// Initialize the Arduino
+void setup() {
+  Serial.begin(9600); // Output serial data specifier
   servo.attach(servoPin);
+
   pinMode(motorPin, OUTPUT);
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
-  pinMode(LEDPin, OUTPUT); // Use LED indicator (if required)
-  
+
   // set up PulseInZero, pass in callback function to be triggered
   // PulseInZero::setup(pulseInComplete);
   // PulseInZero::begin();
 }
 
-void motorPWM(int delayOff, int delayOn) {
-  if (motorOn && motorCounter > delayOn) {
-    digitalWrite(motorPin, LOW);
-    motorCounter = 0;
-    motorOn = false;
-  } else if (!motorOn && motorCounter > delayOff) {
-    digitalWrite(motorPin, HIGH);
-    motorCounter = 0;
-    motorOn = true;
-  }
-  
-} 
- 
-void loop()
-{
-  motorPWM(40, 10); 
-//85 is default
+// Logic that runs continuosly
+void loop() {
 
-  servo.write(85);
+  getSonarData();
 
- digitalWrite(trigPin, LOW); 
- delayMicroseconds(2); 
+  if (distance < maximumRange) {
+    if (!turned) {
+      // We've encountered an object. Turn right and slow down.
+      turnRef(12);
+      changeMotorSpeed(100);
+      delay(100); // Delay a certain period of time for the car to complete a 90-degree turn.
 
- digitalWrite(trigPin, HIGH);
- delayMicroseconds(10); 
- 
- digitalWrite(trigPin, LOW);
- duration = pulseIn(echoPin, HIGH, 1000);
- 
- //Calculate the distance (in cm) based on the speed of sound.
- distance = duration/58.2;
- 
- if (digitalRead(receiver_pin)==LOW ){
-  /* Send a negative number to computer and Turn LED ON 
-  to indicate "out of range" */
-  digitalWrite(LEDPin, HIGH); 
-  if (!turned){
-    servo.write(85);
-    //  delay(1000);
-    turned = true;
-  }
- }
- else {
- /* Send the distance to the computer using Serial protocol, and
- turn LED OFF to indicate successful reading. */
-  Serial.println(distance);
-  digitalWrite(LEDPin, LOW); 
-    servo.write(70);
-    //  delay(1000);
-   turned = false;
- }
- if(motorCounter %2 == 0) { 
-   digitalWrite(LEDPin, LOW); 
+      // We've completed an turn. Drive straight and keep straight until the left IR pin no longer detects the object.
+      turnStraight();
+      while(digitalRead(IRPinLeft) == HIGH) delay(10);
+
+      // We've avoided the object. Return to default state.
+      turnRef(-12);
+      changeMotorSpeed(400);
+    }
   } else {
-    digitalWrite(LEDPin, HIGH);
+    turnStraight();
   }
- //Delay 50ms before next reading.
-  motorCounter++;
+}
+
+/*
+ * Turns the servo a certain angle away from the straight angle.
+ */
+void turnRef(int angle){
+  servo.write(defaultServoAngle + angle);
+  turned = true;
+}
+
+/*
+ * Turns the servo some absolute angle
+ */ 
+void turnAbs(int angle){
+  servo.write(angle);
+  turned = true;
+}
+
+/*
+ * Turns the servo to its default straight position.
+ */ 
+void turnStraight() {
+  servo.write(defaultServoAngle);
+  turned = false;
+}
+
+/*
+ * Changes the motor speed from an input RPM.
+ */ 
+void changeMotorSpeed(int rpm){
+  int speed8bit = rpm / bitToMotorRPM;
+  // Cannot have values greater than 255 or less than 0.
+  if (speed8bit > 255) speed8bit = 255;
+  if (speed8bit < 0) speed8bit = 0;
+  analogWrite(motorPin, speed8bit);
 }
 
 /**
-* Pulse complete callback hanlder for PulseInZero
-* @param duration - pulse length in microseconds
-*/
+ * Pules a wave in the sonar and retrieves the distance.
+ */
+void getSonarData() {
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH, 1000);
+
+  //Calculate the distance (in cm) based on the speed of sound.
+  distance = duration / 58.2;
+}
+
+/**
+ * Pulse complete callback hanlder for PulseInZero
+ * @param duration - pulse length in microseconds
+ */
 // void pulseInComplete(unsigned long duration){
 //   // note: if you're detecting a lot of pulses a second it's probably best to remove the serial prints... 
 //   // (this function is triggered by an interrupt so the serial buffer can fill up and cause the program to hang)
 //   Serial.print("pulse complete - duration: ");
 //   Serial.print(duration);
 //   Serial.println(" us");
-  
+
 //   PulseInZero::begin();  // Start listening again...  
-  
+
 // }
